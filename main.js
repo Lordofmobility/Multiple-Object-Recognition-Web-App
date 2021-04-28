df86bc2350164fafbf35938cf1ae00b0f0f5e548
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-28 20:38:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 img = ""
+status = ""
 
 function setup() {
     canvas = createCanvas(640, 420)
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Object Detected"
+}
+
+function modelLoaded() {
+    console.log("Object Detector Loaded");
+    status = true
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error)
+    }
+    console.log(results);
 }
 
 function preload() {
@@ -15,5 +31,17 @@ function draw() {
     text("Dog", 45, 75)
     noFill()
     stroke("#0000FF")
-    rect(40, 65, 350, 350)
+    rect(40, 60, 350, 350)
+
+    fill("#0000FF")
+    text("Cat", 300, 90)
+    noFill()
+    stroke("#0000FF")
+    rect(295, 75, 275, 315)
+
+    fill("#0000FF")
+    text("Bowl", 250, 325)
+    noFill()
+    stroke("#0000FF")
+    rect(240, 310, 150, 100)
 }
