2eadcb45f9f754ce9dd444cb8457624d3a769905
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-30 20:48:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 img = ""
 status = ""
+objects = []
 
 function setup() {
     canvas = createCanvas(640, 420)
     canvas.center();
     objectDetector = ml5.objectDetector("cocossd", modelLoaded)
-    document.getElementById("status").innerHTML = "Status: Object Detected"
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
 }
 
 function modelLoaded() {
@@ -19,6 +20,7 @@ function gotResult(error, results) {
         console.log(error)
     }
     console.log(results);
+    objects = results
 }
 
 function preload() {
@@ -27,21 +29,33 @@ function preload() {
 
 function draw() {
     image(img, 0, 0, 640, 420)
-    fill("#0000FF")
-    text("Dog", 45, 75)
-    noFill()
-    stroke("#0000FF")
-    rect(40, 60, 350, 350)
+    //fill("#0000FF")
+    //text("Dog", 45, 75)
+    //noFill()
+    //stroke("#0000FF")
+    //rect(40, 60, 350, 350)
+//
+    //fill("#0000FF")
+    //text("Cat", 300, 90)
+    //noFill()
+    //stroke("#0000FF")
+    //rect(295, 75, 275, 315)
+//
+    //fill("#0000FF")
+    //text("Bowl", 250, 325)
+    //noFill()
+    //stroke("#0000FF")
+    //rect(240, 310, 150, 100)
 
-    fill("#0000FF")
-    text("Cat", 300, 90)
-    noFill()
-    stroke("#0000FF")
-    rect(295, 75, 275, 315)
-
-    fill("#0000FF")
-    text("Bowl", 250, 325)
-    noFill()
-    stroke("#0000FF")
-    rect(240, 310, 150, 100)
+    if (status != "") {
+        for (let i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objects Detected"
+            fill("#0000FF")
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15)
+            noFill()
+            stroke("#0000FF")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height)
+        }
+    }
 }
